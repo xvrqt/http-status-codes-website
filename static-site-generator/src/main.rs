d3b521746288731717cf7a5c006f3c9a56181bba
xvrqt <git@xvrqt.com>
2025-02-24 12:13:57 -0800

    Parsing a the errors file with one example succcessfully.

--- static-site-generator/src/main.rs
+++ static-site-generator/src/main.rs
@@ -1,19 +1,23 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
+use std::fs;
+use std::vec::Vec;
 
 /* The JSON file full of structured HTTP error code infomation */
 const HTTP_ERROR_JSON_FILE: &str = "errors.json";
 
 /* The JSON will provide an array of HTTP error information */
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct HttpError {
     code: String,
     reason: String,
-    descriptions: String,
+    description: String,
     link: String,
 }
 
-
 fn main() {
-    println!("Hello, world!");
+    /* Open and read the errors JSON from file */
+    let errors = fs::read_to_string(HTTP_ERROR_JSON_FILE).unwrap();
+    let errors: Vec<HttpError> = serde_json::from_str(&errors).unwrap();
+    println!("{:#?}", errors);
 }
