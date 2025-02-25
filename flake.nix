9da51f6fc9c19f7ae5333ea82d0e932a9ebdd1f8
xvrqt <git@xvrqt.com>
2025-02-25 10:16:56 -0800

    Moved reference.html to the web/ directory. Refactored the flake to include a metadata output attribute set.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Website for generating and learning about HTTP error codes.";
+  description = "Website for generating and learning about HTTP status codes.";
 
   inputs = {
     rust.url = "git+https://git.irlqt.net/crow/rust-flake.git";
@@ -8,46 +8,92 @@
   };
 
   outputs = { rust, utils, nixpkgs, ... }:
-    utils.lib.eachDefaultSystem
-      (system:
-        let
-          overlays = [ rust.overlays.oxalica rust.overlays.default ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
-        in
-        {
-          packages = { };
-          nixosModules = { };
-
-          devShells = {
-            default = pkgs.mkShell {
-              buildInputs = [
-                # To easily serve and test the static webpages
-                pkgs.miniserve
-                # To build and run the template engine that regenerates the website
-                pkgs.rust-toolchain
-              ];
-
-              shellHook = ''
-                project_directory=$(pwd)/web
-                clear
-                if pgrep -x miniserve >> /dev/null
-                then
-                  echo "Server already running."
-                else
-                  # Start the server, set a trap to kill it on exit
-                  miniserve -p 1234 -d monokai static-site-generator/result/ > logs/server.log 2>&1 &
-                  WEB_PID=$!
-                  trap "kill -9 $WEB_PID" EXIT
-                  echo "Server is running at: localhost:1234"
-                  echo "Logs file started at: logs/server.log"
-                fi
-              '';
+    let
+      wrapped = utils.lib.eachDefaultSystem
+        (system:
+          let
+            overlays = [ rust.overlays.oxalica rust.overlays.default ];
+            pkgs = import nixpkgs {
+              inherit system overlays;
             };
-          };
-        }
-      );
+
+          in
+          {
+            packages =
+              let
+                pkgName = "http-status-codes";
+              in
+              {
+                website = pkgs.stdenv.mkDerivation
+                  {
+                    name = "website-${pkgName}";
+                    src = ./static-site-generator;
+
+                    buildInputs = [
+                      # To build and run the template engine that regenerates the website
+                      pkgs.rust-toolchain
+                    ];
+
+                    buildPhase = ''
+                      cargo run --release
+                    '';
+
+                    installPhase = ''
+                      cp -r $src $out
+                    '';
+                  };
+              };
+
+            nixosModules = { };
+
+            devShells = {
+              default = pkgs.mkShell {
+                buildInputs = [
+                  # To easily serve and test the static webpages
+                  pkgs.miniserve
+                  # To build and run the template engine that regenerates the website
+                  pkgs.rust-toolchain
+                ];
+
+                shellHook = ''
+                  # Rebuild the website
+                  cd static-site-generator
+                  cargo run
+                  cd ..
+                  clear
+                  # Check if the server is already working
+                  if pgrep -x miniserve >> /dev/null
+                  then
+                    echo "Server already running."
+                  else
+                    # Start the server, set a trap to kill it on exit
+                    miniserve -p 1234 -d monokai static-site-generator/result/ > logs/server.log 2>&1 & 
+                    WEB_PID=$!
+                    trap "kill -9 $WEB_PID" EXIT
+                    echo "Server is running at: localhost:1234"
+                    echo "Logs file started at: logs/server.log"
+                  fi
+                '';
+              };
+            };
+          }
+        );
+      packages = wrapped.packages;
+      devShells = wrapped.devShells;
+    in
+    {
+      inherit packages devShells;
+      /* So other flakes (i.e. website-flake) can create options and configs for this flake */
+      metadata = rec {
+        name = "http-status-codes";
+        pkgName = "website-${name}";
+        description = "A website which generates HTTP status codes and provides information webpages about them";
+        defaults = {
+          subDomain = "http";
+          domain = "${defaults.subDomain}.xvrqt.com";
+        };
+      };
+    };
 }
 
 
